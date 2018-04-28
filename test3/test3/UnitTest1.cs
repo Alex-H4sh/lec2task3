70af8f0b626ae51c8787ea3a150373736acbd660
AKartashov <AKartashov@AKARTASHOV>
2018-04-28 13:35:33 +0300

    task 3 ready

---
+++ test3/test3/UnitTest1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace test3
+{
+    [TestClass]
+    public class UnitTest
+    {
+        static IWebDriver driver;
+
+        [TestInitialize]
+        public void init()
+        {
+            driver = new ChromeDriver();
+        }
+
+        [TestMethod]
+        public void lec2test3()
+        {
+            driver.Navigate().GoToUrl("https://localhost/litecart/admin");
+            driver.FindElement(By.Name("username")).SendKeys("admin");
+            driver.FindElement(By.Name("password")).SendKeys("admin");
+            driver.FindElement(By.Name("login")).Click();
+            IWebElement success = driver.FindElement(By.CssSelector(".notice.success"));
+            Assert.IsFalse(!success.Text.Equals("You are now logged in as admin"), "You are not admin");
+        }
+
+        [TestCleanup]
+        public void end()
+        {
+            driver.Close();
+        }
+    }
+}
